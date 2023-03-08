5961368bea96a0e441a562cd6abf6285348f428e
Roman Glushko <roman.glushko.m@gmail.com>
2023-03-08 18:22:05 +0200

    Initial Commit

---
+++ iDine/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  iDine
+//
+//  Created by Roman Glushko on 08.03.2023.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ iDine/iDineApp.swift
@@ -0,0 +1,17 @@
+//
+//  iDineApp.swift
+//  iDine
+//
+//  Created by Roman Glushko on 08.03.2023.
+//
+
+import SwiftUI
+
+@main
+struct iDineApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
